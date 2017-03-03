20dea1ffde64ce8b62b8a4ca7eb34ce39670d18f
winlet <stella>
2017-03-03 21:29:25 +0300

    added files

---
+++ messanger/db/seeds.rb
@@ -0,0 +1,11 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+m1 = Message.create(content:'love u')
+m2 = Message.create(content:'miss u')
+m3 = Message.create(content:'waiting for u')
+m4 = Message.create(content:'seeing u')
